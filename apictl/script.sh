3baf39b9b4ac827d42f7164fa3da04fb343c5e15
Jose Joaquin de la Gala <jjgala@minsait.com>
2021-02-08 17:09:30 +0100

    apictl login.

--- apictl/script.sh
+++ apictl/script.sh
@@ -1,2 +1,3 @@
-./apictl add-env -e dev --apim https://apim-with-analytics_mysql_1:9443
+./apictl add-env -e dev --apim https://172.18.0.1:9443
 ./apictl login dev -u admin -p admin -k
+./apictl list apis -e dev -k
